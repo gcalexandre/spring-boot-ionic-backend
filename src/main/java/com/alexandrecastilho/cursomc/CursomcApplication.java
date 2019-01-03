0759b52c324c094ed61f5baae9d839f0cb74d8cc
Alexandre Castilho Linux <gcalexandre.castilho@gmail.com>
2019-01-03 18:15:34 -0200

    Pedido, EstadoPagemento e Pagamento

--- src/main/java/com/alexandrecastilho/cursomc/CursomcApplication.java
+++ src/main/java/com/alexandrecastilho/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.alexandrecastilho.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,25 @@ import com.alexandrecastilho.cursomc.domain.Cidade;
 import com.alexandrecastilho.cursomc.domain.Cliente;
 import com.alexandrecastilho.cursomc.domain.Endereco;
 import com.alexandrecastilho.cursomc.domain.Estado;
+import com.alexandrecastilho.cursomc.domain.Pagamento;
+import com.alexandrecastilho.cursomc.domain.PagamentoComBoleto;
+import com.alexandrecastilho.cursomc.domain.PagamentoComCartao;
+import com.alexandrecastilho.cursomc.domain.Pedido;
 import com.alexandrecastilho.cursomc.domain.Produto;
+import com.alexandrecastilho.cursomc.domain.enums.EstadoPagamento;
 import com.alexandrecastilho.cursomc.domain.enums.TipoCliente;
 import com.alexandrecastilho.cursomc.repositories.CategoriaRepository;
 import com.alexandrecastilho.cursomc.repositories.CidadeRepository;
 import com.alexandrecastilho.cursomc.repositories.ClienteRepository;
 import com.alexandrecastilho.cursomc.repositories.EnderecoRepository;
 import com.alexandrecastilho.cursomc.repositories.EstadoRepository;
+import com.alexandrecastilho.cursomc.repositories.PagamentoRepository;
+import com.alexandrecastilho.cursomc.repositories.PedidoRepository;
 import com.alexandrecastilho.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
@@ -36,44 +44,48 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Infórmatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
-		Produto p1 = new Produto(null,  "Computador", 2000.00);
-		Produto p2 = new Produto(null,  "Impressora", 1000.00);
-		Produto p3 = new Produto(null,  "Mouse", 2000.00);
-		
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 1000.00);
+		Produto p3 = new Produto(null, "Mouse", 2000.00);
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p2.getCategorias().addAll(Arrays.asList(cat1));
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2 ));
-		produtoRepository.saveAll(Arrays.asList(p1, p2 , p3));
-		
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 		Estado est1 = new Estado(null, "Minas gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
+
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
@@ -85,7 +97,23 @@ public class CursomcApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
-
