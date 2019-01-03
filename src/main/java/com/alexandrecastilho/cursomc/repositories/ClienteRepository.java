91f3ee1cabb70a92822a5235ee709cdc4aaf54ec
Alexandre Castilho Linux <gcalexandre.castilho@gmail.com>
2019-01-03 02:33:12 -0200

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/alexandrecastilho/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.alexandrecastilho.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexandrecastilho.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
