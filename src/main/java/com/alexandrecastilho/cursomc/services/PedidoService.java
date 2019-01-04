d271e4be1e2089220abcd63c9eb0ea1156878135
Alexandre Castilho Linux <gcalexandre.castilho@gmail.com>
2019-01-04 03:36:33 -0200

    Endpoint /pedidos-id- disponibilizados

---
+++ src/main/java/com/alexandrecastilho/cursomc/services/PedidoService.java
@@ -0,0 +1,28 @@
+package com.alexandrecastilho.cursomc.services;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandrecastilho.cursomc.domain.Pedido;
+import com.alexandrecastilho.cursomc.repositories.PedidoRepository;
+import com.alexandrecastilho.cursomc.services.exceptions.ObjectNotFoundException;
+
+;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Pedido.class.getName()));
+				
+	}
+}
+
