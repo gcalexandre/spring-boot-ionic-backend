2ceff53712b80ea7695ac254e0921ed7da7fd51d
Alexandre Castilho Linux <gcalexandre.castilho@gmail.com>
2019-01-04 15:15:35 -0200

    Inserindo uma nova Categoria com o POST

--- src/main/java/com/alexandrecastilho/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexandrecastilho/cursomc/services/CategoriaService.java
@@ -24,5 +24,10 @@ public class CategoriaService {
 				"Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
 				
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
 
