c8a84c5e9e3485c0571a321ced98ded55bb3e8dc
Alexandre Castilho Linux <gcalexandre.castilho@gmail.com>
2019-01-04 16:31:26 -0200

    Listando todas as categorias com DTO

--- src/main/java/com/alexandrecastilho/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexandrecastilho/cursomc/services/CategoriaService.java
@@ -1,6 +1,6 @@
 package com.alexandrecastilho.cursomc.services;
 
-
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,28 +24,30 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
-				
+
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e ) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegretyException("Não é possivel excluir uma categoria que possui produtos");
-			
+
 		}
 	}
-}
 
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+}
