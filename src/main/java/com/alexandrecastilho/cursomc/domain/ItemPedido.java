53cc643b1c0eacd14d0d8332061c78d8e9e83a20
gcalexandre <gcalexandre.castilho@gmail.com>
2019-02-08 21:00:39 -0200

    MockEmailService. Padrões Strategy e Template

--- src/main/java/com/alexandrecastilho/cursomc/domain/ItemPedido.java
+++ src/main/java/com/alexandrecastilho/cursomc/domain/ItemPedido.java
@@ -1,28 +1,28 @@
 package com.alexandrecastilho.cursomc.domain;
 
 import java.io.Serializable;
-
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
 
-	public ItemPedido( ) {
-		}
+	public ItemPedido() {
+	}
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
@@ -32,26 +32,25 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
-		
+
 	}
-	
+
 	public void setPedido(Pedido pedido) {
 		id.setPedido(pedido);
 	}
-	
-	
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
 	public void setProduto(Produto produto) {
 		id.setProduto(produto);
 	}
@@ -112,7 +111,23 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qte: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubTotal()));
+		builder.append("\n");
+
+		return builder.toString();
+
+	}
+
 }
---
+++ src/main/java/com/alexandrecastilho/cursomc/services/EmailService.java
@@ -0,0 +1,14 @@
+package com.alexandrecastilho.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.alexandrecastilho.cursomc.domain.Pedido;
+
+public interface EmailService {
+	
+	void sendOrderConfirmationEmail(Pedido obj);
+	void sendEmail(SimpleMailMessage msg);
+	
+	
+
+}
