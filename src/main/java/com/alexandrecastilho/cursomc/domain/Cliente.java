0c97559e1f42f235a1c4ea151f1367c9785375a8
gcalexandre <gcalexandre.castilho@gmail.com>
2019-04-02 23:01:15 -0300

    Adicionando senha a cliente

--- src/main/java/com/alexandrecastilho/cursomc/domain/Cliente.java
+++ src/main/java/com/alexandrecastilho/cursomc/domain/Cliente.java
@@ -34,6 +34,9 @@ public class Cliente implements Serializable {
 	private Integer tipo;
 
 	
+	@JsonIgnore
+	private String senha;
+	
 	@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
@@ -49,13 +52,14 @@ public class Cliente implements Serializable {
 
 	}
 
-	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha)  {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo==null) ? null : tipo.getCod();
+		this.senha = senha;
 	}
 
 	public Integer getId() {
@@ -100,6 +104,14 @@ public class Cliente implements Serializable {
 
 	public List<Endereco> getEnderecos() {
 		return enderecos;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public void setEnderecos(List<Endereco> enderecos) {
--- src/main/java/com/alexandrecastilho/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/alexandrecastilho/cursomc/dto/ClienteNewDTO.java
@@ -29,6 +29,9 @@ public class ClienteNewDTO implements Serializable {
 	private Integer tipo;
 
 	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
@@ -156,5 +159,13 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 }
