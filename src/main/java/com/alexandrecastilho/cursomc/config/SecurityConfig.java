274cfd0b3e9054467b4545d70b80ef5cae3beef0
gcalexandre <gcalexandre.castilho@gmail.com>
2019-04-07 00:41:30 -0300

    Implementando autorizacao

--- src/main/java/com/alexandrecastilho/cursomc/config/SecurityConfig.java
+++ src/main/java/com/alexandrecastilho/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.alexandrecastilho.cursomc.security.JWTAuthenticationFilter;
+import com.alexandrecastilho.cursomc.security.JWTAuthorizationFilter;
 import com.alexandrecastilho.cursomc.security.JWTUtil;
 
 
@@ -59,6 +60,7 @@ import com.alexandrecastilho.cursomc.security.JWTUtil;
 				.antMatchers(PUBLIC_MATCHERS).permitAll()
 				.anyRequest().authenticated();
 			http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+			http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		}
 		
